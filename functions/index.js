fd48d6cd72df5964ca80109d635fe34741dec8d0
raj patel <kakhilesh79@gmail.com>
2020-09-12 11:01:14 +0530

    changes

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,12 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.randomNumber = functions.https.onRequest((request,response)=>{
+    const number = Math.round(Math.random()*100);
+    console.log(number);
+    response.send(number.toString());
+});
+
+exports.toTheDojo = functions.https.onRequest((request,response)=>{
+
+    response.redirect('https://worstcoders.in/');
+});
